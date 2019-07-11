bdf9c3c3327534f75292d1507eed75b11b23aba3
Xuan Nguyen Duc <xuan.nguyen@grabtaxi.com>
2019-07-11 09:41:39 +0700

    add examples

--- palindrome/palindrome_test.go
+++ palindrome/palindrome_test.go
@@ -1,8 +1,19 @@
 package palindrome
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestPalindrome(t *testing.T)  {
+func BenchmarkPalindrome(b *testing.B)  {
+	// If there is any complex initialization, perform it here and call b.ResetTimer to reset timer.
+	// Therefore, it will not add to the measured time of each iteration.
+	for i := 0; i < b.N; i++ {
+		IsPalindrome("aaaa")
+	}
+}
+
+func TestIsPalindrome(t *testing.T)  {
 	testCases := []struct{
 		input string
 		output bool
@@ -18,4 +29,11 @@ func TestPalindrome(t *testing.T)  {
 			t.Errorf("IsPalindrome(%s) != %t", tc.input, tc.output)
 		}
 	}
+}
+
+func ExampleIsPalindrome()  {
+	fmt.Printf("%t", IsPalindrome("aaaa"))
+	// go test will check output is printed into standard output
+	// Output:
+	// true
 }
